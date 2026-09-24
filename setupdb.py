e815939abcb44be3bc6aba1c521017ff81ce64f8
Natarjj <natacelso2000@gmail.com>
2026-09-23 21:50:44 -0300

    salve

--- setupdb.py
+++ setupdb.py
@@ -1,3 +1,17 @@
 import sqlite3
 
-connction = sqlite3.connect("database.db")
+# Abre (ou cria) o banco de dados
+connection = sqlite3.connect("database.db")
+
+# Lê o arquivo SQL modelador do banco de dados
+with open("database.sql", "r", encoding="utf-8") as file:
+    sql = file.read()
+
+# Executa todo o script SQL
+connection.executescript(sql)
+
+# Fecha a conexão
+connection.close()
+
+# Feedback
+print("Banco de dados preparado com sucesso!")
